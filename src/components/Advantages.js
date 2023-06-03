b15fa8e25e666b9a050a9371b2c6d41bb4785fa5
36Kryptum <square2951@gmail.com>
2023-06-03 21:58:01 +0200

    components

--- src/components/Advantages.js
+++ src/components/Advantages.js
@@ -8,19 +8,19 @@ export default function Advantages() {
             <div className="grid grid-cols-1 lg:grid-cols-4 gap-4">
                 <div className="bg-white border-zinc-400 text-accent rounded border dark:border-white p-4 hover:scale-105 duration-300">
                     <h3 className="text-center text-primary my-2 text-xl font-bold">Maßgeschneiderte Lösung</h3>
-                    Du erhältst eine komplett individuellen Webpräsenz, der auf die Bedürfnisse deines Unternehmens abgestimmt ist.
+                    Du erhältst eine komplett individuelle Webpräsenz, die ganz auf die Bedürfnisse deines Unternehmens abgestimmt ist.
                 </div>
                 <div className="bg-white border-zinc-400 text-accent rounded border dark:border-white p-4 hover:scale-105 duration-300">
-                    <h3 className="text-center text-primary my-2 text-xl font-bold">Null versteckten Kosten</h3>
+                    <h3 className="text-center text-primary my-2 text-xl font-bold">Null versteckte Kosten</h3>
                     Du bezahlst lediglich einen monatlichen Fixpreis, du musst also keine Fixkosten für die Erstellung deiner Website aufbringen!
                 </div>
                 <div className="bg-white border border-zinc-400 text-accent roundedborder dark:border-white p-4 hover:scale-105 duration-300">
                     <h3 className="text-center text-primary my-2 text-xl font-bold">Keine Kopfschmerzen</h3>
-                    Du bekommst das Rundum-Sorglos-Paket. Ich kümmere mich um alles von der Erstellung über die Online-Schaltung, bis hin zur Wartung, Anpassung und Optimiereung. Somit kannst du dich voll und ganz auf dein Geschäft konzentrieren.
+                    Du bekommst das Rundum-Sorglos-Paket. Ich kümmere mich um alles von der Erstellung, über die Online-Schaltung, bis hin zur Wartung, Anpassung und Optimierung. Somit kannst du dich voll und ganz auf dein Geschäft konzentrieren.
                 </div>
                 <div className="bg-white border-zinc-400 text-accent rounded border dark:border-white p-4 hover:scale-105 duration-300">
-                    <h3 className="text-center text-primary my-2 text-xl font-bold">kompetente Betreuung</h3>
-                    Du bist nachhaltig für die Zukunft gerüstet. Wenn Inhalte oder Design geändert werden sollen, setze ich diese schnell und umkompliziert um
+                    <h3 className="text-center text-primary my-2 text-xl font-bold">Kompetente Betreuung</h3>
+                    Du bist nachhaltig für die Zukunft gerüstet. Wenn Inhalte oder Design geändert werden sollen, setze ich diese schnell und umkompliziert um.
                 </div>
             </div>
         </div>
