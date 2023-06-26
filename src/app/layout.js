cabc5af223ba51a0abe8057cf9226974465ab132
Marvin Steinborn - I21 <marvin.steinborn@nordakademie.de>
2023-06-26 09:47:58 +0200

    added refactoring ui colors

--- src/app/layout.js
+++ src/app/layout.js
@@ -12,7 +12,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="de" suppressHydrationWarning>
-      <body className='bg-white text-accent dark:bg-background-dark dark:text-white'>
+      <body className='bg-white text-cool-grey-500 dark:bg-background-dark dark:text-white'>
 
         <Providers>
           <NavBar/>
--- src/components/Hero.js
+++ src/components/Hero.js
@@ -8,12 +8,12 @@ export default function Hero() {
     <div className="container flex flex-col justify-center p-6 mx-auto sm:py-12 lg:py-24 lg:flex-row lg:justify-between">
       <div className="flex flex-col justify-center text-center rounded-sm lg:max-w-md xl:max-w-2xl lg:text-left">
         <h1 className="text-5xl font-bold leading-none text-background-dark dark:text-white sm:text-6xl">Schaffe <br/>
-          <span className="text-primary transition duration-300 hover:translate-x-8">digitalen Erfolg </span>für dein Unternehmen
+          <span className="text-orange-vivid-400 transition duration-300 hover:translate-x-8">digitalen Erfolg </span>für dein Unternehmen
         </h1>
         <p className="my-6 text-xl">Hebe dich von deiner Konkurrenz ab. Mit einer individuellen Website im Abomodell, ganz ohne Intitialkosten.</p>
   
         <div className="flex flex-col space-y-4 sm:items-center sm:justify-center sm:flex-row sm:space-y-0 sm:space-x-4 lg:justify-start">
-          <a rel="noopener noreferrer" onClick={handleClick} className="px-8 py-3 text-lg text-white cursor-pointer font-semibold rounded bg-primary dark:text-background-dark">Kontakt aufnehmen</a>
+          <a rel="noopener noreferrer" onClick={handleClick} className="px-8 py-3 text-lg text-white cursor-pointer font-semibold rounded bg-orange-vivid-400 dark:text-background-dark">Kontakt aufnehmen</a>
           <a rel="noopener noreferrer" onClick={handleClick} className="px-8 py-3 text-lg font-semibold cursor-pointer border border-background-dark rounded dark:border-gray-100">mehr erfahren</a>
         </div>
       </div>
